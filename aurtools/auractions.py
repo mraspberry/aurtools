03d8d7d6264c0c98affc4ec2dd060694edd9c435
Matthew Raspberry <nixalot@nixalot.com>
2015-01-25 10:53:16 -0600

    fixed bug in auractions:downloadPkg

--- aurtools/auractions.py
+++ aurtools/auractions.py
@@ -1,32 +1,9 @@
-#!/usr/bin/env python
-"""
-This script searches the Arch Linux AUR for the provided package names and prints out all results returned
-
-Author: Matthew Raspberry
-Email: nixalot[at]nixalot[dot]com
-
-Copyright 2013 Matthew Raspberry
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 import requests
 import tarfile
 import tempfile
 
-AURBASE = 'https://aur.archlinux.org'
-_AURRPC = '{base}/rpc.php'.format(base=AURBASE)
+_AURBASE = 'https://aur.archlinux.org'
+_AURRPC = '{base}/rpc.php'.format(base=_AURBASE)
 
 def _get_results(resultobj):
     return resultobj.json()['results']
